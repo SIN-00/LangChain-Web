e2d2fbd8326bffa1c5f2f283625081d687ebe86c
SHIN__0.0 <ajtwoddltom@gmail.com>
2025-03-19 19:20:09 +0900

    view connect

--- poet/main.py
+++ poet/main.py
@@ -1,14 +1,16 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-from langchain_openai import OpenAI
+from langchain_openai import ChatOpenAI 
+import streamlit as st
 
-llm = OpenAI()
-result = llm.invoke("hi!")
-print(result)
+chat_model = ChatOpenAI()
 
-# from langchain_openai import ChatOpenAI 
 
-# chat_model = ChatOpenAI()
-# result = chat_model.invoke("hi!")
-# print(result.content)
+st.title("인공지능 시인")
+content = st.text_input("시의 주제를 제시해주세요")
+
+if st.button("시 작성 요청하기"):
+    with st.spinner("시 작성중...", show_time=True):
+        result = chat_model.invoke(content + "에 대한 시를 써줘")
+        st.write(result.content)
