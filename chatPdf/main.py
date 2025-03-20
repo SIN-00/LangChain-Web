0aee19db2f44b9cc7c2ae2c25da6844a307f14a6
SHIN__0.0 <ajtwoddltom@gmail.com>
2025-03-20 16:58:26 +0900

    split

--- chatPdf/main.py
+++ chatPdf/main.py
@@ -1,6 +1,16 @@
 from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 loader = PyPDFLoader("economic.pdf")
 pages = loader.load_and_split()
 
-print(pages[200])
+text_splitter = RecursiveCharacterTextSplitter(
+    # Set a really small chunk size, just to show.
+    chunk_size=100,
+    chunk_overlap=20, #겹치는 글자정해서 문맥
+    length_function=len,
+    is_separator_regex=False,
+)
+texts = text_splitter.split_documents(pages)
+
+print(texts[0])
